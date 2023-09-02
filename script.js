8f7ad9053f90dff1f57c075aae8136aefdcf728c
Sanoj Ahamed <sanojahamed1730@gmail.com>
2023-09-02 20:46:01 +0530

    Js Update

--- script.js
+++ script.js
@@ -2,38 +2,39 @@ let movies = [
     {
 name: "falcon and the winter soldier",
 des :
-"Loki is an American television series created by Michael Waldron for the streaming service Disney."
-image: "images/slider 2.png"
+"Loki is an American television series created by Michael Waldron for the streaming service Disney.",
+image: "./images/nxt.png"
     },
-    
-    {
-        name: "falcon and the winter soldier",
-        des :
-        "Loki is an American television series created by Michael Waldron for the streaming service Disney."
-        image: "images/slider 2.png"
-            },
-            {
-                name: "falcon and the winter soldier",
-                des :
-                "Loki is an American television series created by Michael Waldron for the streaming service Disney."
-                image: "images/slider 2.png"
-                    },
-                    {
-                        name: "falcon and the winter soldier",
-                        des :
-                        "Loki is an American television series created by Michael Waldron for the streaming service Disney."
-                        image: "images/slider 2.png"
-                            }];
+
+];
                             
-const carousel= document.querySelector(".carousel");
+const carousel= document.querySelector('.carousel');
 let sliders=[];
 
-let sliderIndex=0; //track the current slide
+let slideIndex=0; //track the current slide
 
 const createSlide = () =>  {
     if(slideIndex>=movies.lenght) {
         slideIndex=0;
     }
+
+//create DOM Elements
+let slide= document.createElement('div');
+var imgElement = document.createElement('img');
+let content= document.createElement('div');
+let h1= document.createElement('h1');
+let p= document.createElement('p');
+
+
+//attaching all element
+imgElement.appendChild(document.createTextNode(''));
+h1.appendChild(document.createTextNode(movies[slideIndex].name));
+p.appendChild(document.createTextNode(movies[slideIndex].des));
+content.appendChild(h1);
+content.appendChild(p);
+sliders.appendChild(imgElement);
+sliders.appendChild(content);
+carousel.appendChild(slide);
+
 }
 
-//create 
