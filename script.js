afe9a325ede1d25f770a71c83df8e4e2c858ab15
Sanoj Ahamed <sanojahamed1730@gmail.com>
2023-09-04 01:17:07 +0530

    hotstar clone update

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 let movies = [
-    
     {
         name: "loki",
         des: "Loki is an American television series created by Michael Waldron for the streaming service Disney.",
-        image: "./images/slider 1.PNG"
+        image: "./images/slider 1.PNG",
     },
     {
         name: "falcon and the winter soldier",
         des: "Falcon and the Winter Soldier is an American television series created for the streaming platform Disney+.",
-        image: "./images/slider 2.PNG"
+        image: "./images/slider 2.PNG",
     },
     {
         name: "WandaVision",
         des: "WandaVision is an American television series created for the streaming service Disney+",
-        image: "./images/slider 3.PNG"
+        image: "./images/slider 3.PNG",
     },
 
     {
         name: "Raya and the Last Dragon",
         des: "Raya and the Last Dragon is an animated Disney film released in 2021.",
-        image: "./images/slider 4.PNG"
+        image: "./images/slider 4.PNG",
     },
 
     {
         name: "Luca",
         des: "Luca is a Disney-Pixar animated film released in 2021.",
-        image: "./images/slider 5.PNG"
-    }
+        image: "./images/slider 5.PNG",
+    },
 ];
 
-const carousel = document.querySelector('.carousel');
+const carousel = document.querySelector(".carousel");
 let sliders = [];
 
 let slideIndex = 0; // track the current slide
@@ -40,23 +39,20 @@ const createSlide = () => {
     }
 
     // create DOM Elements
-    let slide = document.createElement('div');
-    let imgElement = document.createElement('img');
-    let content = document.createElement('div');
-    let h1 = document.createElement('h1');
-    let p = document.createElement('p');
+    let slide = document.createElement("div");
+    let imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
 
-    
-    // attaching all elements
-   
+    // attaching all element
+    imgElement.appendChild(document.createTextNode(""));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     p.appendChild(document.createTextNode(movies[slideIndex].des));
-
-    // Attach elements to the slide and carousel
     content.appendChild(h1);
     content.appendChild(p);
-    slide.appendChild(imgElement);
     slide.appendChild(content);
+    slide.appendChild(imgElement);
     carousel.appendChild(slide);
 
     // setting up images
@@ -68,16 +64,14 @@ const createSlide = () => {
     content.className = "slide-content";
     h1.className = "movie-title";
     p.className = "movie-des";
-   
+
     sliders.push(slide);
-    
 
     if (sliders.length) {
-        sliders[0].style.marginLeft =  `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)
-        }px)`;
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)
+            }px)`;
     }
-    
-    }
+};
 
 for (let i = 0; i < 3; i++) {
     createSlide();
@@ -86,3 +80,37 @@ for (let i = 0; i < 3; i++) {
 setInterval(() => {
     createSlide();
 }, 3000);
+
+// video cards
+
+const videoCards = document.querySelectorAll(".video-card");
+
+videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        let video = item.children[1];
+        video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+        let video = item.children[1];
+        video.pause();
+    });
+});
+
+// cards sliders
+
+let cardContainers = document.querySelectorAll(".card-container");
+let preBtns = document.querySelectorAll(".pre-btn");
+let nxtBtns = document.querySelectorAll(".nxt-btn");
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener("click", () => {
+        item.scrollLeft += containerWidth - 200;
+    });
+
+    preBtns[i].addEventListener("click", () => {
+        item.scrollLeft -= containerWidth + 200;
+    });
+});
